7376aba39000c232986e714fc48ad8ee66765093
guarref <prostoboh902@gmail.com>
2025-02-12 17:26:56 +0300

    with CRUD handlers

--- Docker_DB/main.go
+++ Docker_DB/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -26,7 +27,40 @@ func CreateMessage(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 	DB.Create(&taska)                 // отправили в БД структуру Task
-	json.NewEncoder(rw).Encode(taska) // обратно кодируем в json и отправляем клиенту
+	json.NewEncoder(rw).Encode(taska) // обратно кодируем в json и отправляем клиенту, чтобы показать, что все записалось корректно
+}
+
+func PatchMessage(rw http.ResponseWriter, r *http.Request) {
+	var newtask Task
+	err := json.NewDecoder(r.Body).Decode(&newtask)
+	if err != nil {
+		http.Error(rw, "Неверный json", http.StatusBadRequest)
+		return
+	}
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	var oldtask Task
+	result := DB.First(&oldtask, id)
+	if result.Error != nil {
+		http.Error(rw, "Нет такой таски", http.StatusNotFound)
+		return
+	}
+	oldtask.Task, oldtask.IsDone = newtask.Task, newtask.IsDone
+	DB.Save(&oldtask)
+	json.NewEncoder(rw).Encode(oldtask)
+}
+
+func DeleteMessage(rw http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	var deltask Task
+	result := DB.First(&deltask, id)
+	if result.Error != nil {
+		http.Error(rw, "Нет такой таски", http.StatusNotFound)
+		return
+	}
+	DB.Delete(&deltask)
+	rw.WriteHeader(http.StatusNoContent)
 }
 
 func main() {
@@ -39,5 +73,7 @@ func main() {
 	router := mux.NewRouter() // создаем роутер для обработки запросов
 	router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
 	router.HandleFunc("/api/messages", GetMessages).Methods("GET")
+	router.HandleFunc("/api/messages/{id}", PatchMessage).Methods("PATCH")
+	router.HandleFunc("/api/messages/{id}", DeleteMessage).Methods("DELETE")
 	http.ListenAndServe(":8080", router)
 }
