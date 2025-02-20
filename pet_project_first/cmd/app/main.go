93c7a3150b165107a137aec5df77e809da6a6079
guarref <prostoboh902@gmail.com>
2025-02-20 23:22:36 +0300

    with added makefile

--- pet_project_first/cmd/app/main.go
+++ pet_project_first/cmd/app/main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	database.InitDB()
-	database.DB.AutoMigrate(&taskService.Task{})
+	// database.DB.AutoMigrate(&taskService.Task{}) - без makefile
 
 	repo := taskService.NewTaskRepository(database.DB)
 	service := taskService.NewService(repo)
